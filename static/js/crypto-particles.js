6f394aab8ca03a2f23a12d3009feda9926ad5831
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-25 11:35:36 +0100

    Create crypto-particles.js

---
+++ static/js/crypto-particles.js
@@ -0,0 +1,21 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 40 },
+    "shape": {
+      "type": "image",
+      "image": [
+        { "src": "https://cryptologos.cc/logos/bitcoin-btc-logo.png", "width": 20, "height": 20 },
+        { "src": "https://cryptologos.cc/logos/ethereum-eth-logo.png", "width": 20, "height": 20 },
+        { "src": "https://cryptologos.cc/logos/tether-usdt-logo.png", "width": 20, "height": 20 }
+      ]
+    },
+    "opacity": { "value": 0.8 },
+    "size": { "value": 20, "random": true },
+    "move": { "enable": true, "speed": 2, "direction": "none", "random": true }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" }
+    }
+  }
+});
